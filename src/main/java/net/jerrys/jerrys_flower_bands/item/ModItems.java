6331973c5ed98c08e682cf9a67fa12df2fb05036
Jérôme Bucquet <jerry.bucquet@gmail.com>
2025-03-15 18:11:02 +0100

    feat: make flower bands wearable

--- src/main/java/net/jerrys/jerrys_flower_bands/item/ModItems.java
+++ src/main/java/net/jerrys/jerrys_flower_bands/item/ModItems.java
@@ -1,6 +1,7 @@
 package net.jerrys.jerrys_flower_bands.item;
 
 import net.jerrys.jerrys_flower_bands.JerrysFlowerBands;
+import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Item.Properties;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -18,11 +19,11 @@ public class ModItems {
                         JerrysFlowerBands.MOD_ID);
 
         public static final RegistryObject<Item> DAISY_FLOWER_BAND = ITEMS.register("daisy_flower_band",
-                        () -> new Item(new Properties()));
+                        () -> new ArmorItem(FlowerBandMaterials.DAISY, ArmorItem.Type.HELMET, new Properties()));
         public static final RegistryObject<Item> DANDELION_FLOWER_BAND = ITEMS.register("dandelion_flower_band",
-                        () -> new Item(new Properties()));
+                        () -> new ArmorItem(FlowerBandMaterials.DANDELION, ArmorItem.Type.HELMET, new Properties()));
         public static final RegistryObject<Item> POPPY_FLOWER_BAND = ITEMS.register("poppy_flower_band",
-                        () -> new Item(new Properties()));
+                        () -> new ArmorItem(FlowerBandMaterials.POPPY, ArmorItem.Type.HELMET, new Properties()));
 
         public static void register(IEventBus eventBus) {
                 ITEMS.register(eventBus);
