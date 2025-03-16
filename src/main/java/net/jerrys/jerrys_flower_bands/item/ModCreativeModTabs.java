021e04ed23b31db545fdeb163cc2224192abc362
Jérôme Bucquet <jerry.bucquet@gmail.com>
2025-03-16 11:46:03 +0100

    fix: set sunfire flower band as tap icon

--- src/main/java/net/jerrys/jerrys_flower_bands/item/ModCreativeModTabs.java
+++ src/main/java/net/jerrys/jerrys_flower_bands/item/ModCreativeModTabs.java
@@ -16,24 +16,21 @@ public class ModCreativeModTabs {
     throw new UnsupportedOperationException("This is a utility class and cannot be instantiated");
   }
 
-  public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
-      DeferredRegister.create(Registries.CREATIVE_MODE_TAB, JerrysFlowerBands.MOD_ID);
+  public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister
+      .create(Registries.CREATIVE_MODE_TAB, JerrysFlowerBands.MOD_ID);
 
-  public static final RegistryObject<CreativeModeTab> JERRYS_FLOWER_BANDS_TAB =
-      CREATIVE_MODE_TABS.register(
-          "jerrys_flower_bands_tab",
-          () ->
-              CreativeModeTab.builder()
-                  .icon(() -> new ItemStack(ModItems.BLUSH_FLOWER_BAND.get()))
-                  .title(Component.translatable("creativetab.jerrys_flower_bands_tab"))
-                  .displayItems(
-                      (pParameters, pOutput) -> {
-                        for (RegistryObject<Item> itemRegistryObject :
-                            ModItems.ITEMS.getEntries()) {
-                          pOutput.accept(itemRegistryObject.get());
-                        }
-                      })
-                  .build());
+  public static final RegistryObject<CreativeModeTab> JERRYS_FLOWER_BANDS_TAB = CREATIVE_MODE_TABS.register(
+      "jerrys_flower_bands_tab",
+      () -> CreativeModeTab.builder()
+          .icon(() -> new ItemStack(ModItems.SUNFIRE_FLOWER_BAND.get()))
+          .title(Component.translatable("creativetab.jerrys_flower_bands_tab"))
+          .displayItems(
+              (pParameters, pOutput) -> {
+                for (RegistryObject<Item> itemRegistryObject : ModItems.ITEMS.getEntries()) {
+                  pOutput.accept(itemRegistryObject.get());
+                }
+              })
+          .build());
 
   public static void register(IEventBus eventBus) {
     CREATIVE_MODE_TABS.register(eventBus);
